751978c2c08edc8dd39f00ac2548557a574f9420
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-01 10:54:13 -0600

    orders.js is complete

--- orders.js
+++ orders.js
@@ -30,7 +30,19 @@
 */
 
 //CODE HERE
+class Ticket {
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';
+    }
 
+    updateStatus(newStatus){
+        this.status = newStatus;
+        console.log(`the order for customer ${this.customerId} is now ${this.status}.`)
+    }
+}
 
 
 /*
@@ -45,7 +57,8 @@
 */
 
 //CODE HERE
-
+const firstTicket = new Ticket(['pizza','bread','soda'],'7:03 PM',575);
+// console.log(firstTicket);
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +67,5 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking');
+// console.log(firstTicket);
